022c54ca724837e3a61fbee6503690ff8d265370
Alexgreen92 <alexgreen2015@icloud.com>
2018-11-14 18:20:43 -0600

    looks like its done

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # unit-4-game
+Here's how the app works:
+
+   * When the game starts, you can choose a character by clicking on the fighter's picture. You will fight as that character for the rest of the game.
+
+   * You  must then defeat all of the remaining fighters. Enemies should be moved to a different area of the screen.
+
+   * You choose an opponent by clicking on an enemy's picture.
+
+   * Once you select an opponent, that enemy is moved to a `defender area`.
+
+   * You will now be able to click the `attack` button.
+     * Whenever you clicks `attack`, your character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture. 
+     * The opponent character will instantly counter the attack. When that happens, the your character will lose some of their `HP`. These points are shown at the bottom of the your character's picture.
+    
+You will keep hitting the attack button in an effort to defeat their opponent.
+
+   * When the defender's `HP` is reduced to zero or below, enemy is gonna be removed from the `defender area`. You will be able to a new opponent.
+
+You win the game by defeating all enemy characters. You lose the game if your character's `HP` falls to zero or below and while there are still enemies remaining.
---
+++ assets/javascript/game.js
@@ -0,0 +1,79 @@
+
+var pikachu={
+    name:'Pikachu',
+    healthPoints:140,
+    attackPower:10,
+    counterAttackPower:17,
+}
+var bulbasaur={
+    name:'Bulbasaur',
+    healthPoints:150,
+    attackPower:9,
+    counterAttackPower:19,
+}
+var charmander={
+    name:'Charmander',
+    healthPoints:120,
+    attackPower:13,
+    counterAttackPower:18,
+}
+var psyduck={
+    name:'Psyduck',
+    healthPoints:115,
+    attackPower:21,
+    counterAttackPower:16,
+}
+characterChosen=false;
+enemyChosen=false;
+var char;
+var enemy;
+var baseIncrease;
+var enemiesDefeated=0;
+$(document).ready(function() {
+
+$('.character').on('click',function(){
+    if(!characterChosen){
+    $('#yourCharacter').html(this)
+    if(this.id==1){char=pikachu}else if(this.id==2){char=bulbasaur}else if(this.id==3){char=charmander}else{char=psyduck}
+    characterChosen=true;
+    baseIncrease=char.attackPower;
+    $('#characters').appendTo('#enemies')
+    }else if(!enemyChosen&&this.id!=$('#yourCharacter .character').attr('id')){
+    $('#defender').html(this)
+    if(this.id==1){enemy=pikachu}else if(this.id==2){enemy=bulbasaur}else if(this.id==3){enemy=charmander}else{enemy=psyduck}
+    enemyChosen=true;
+    }
+})
+$('#attack').on('click',function(){
+    if(characterChosen&&enemyChosen){
+    enemy.healthPoints-=char.attackPower;
+    char.healthPoints-=enemy.counterAttackPower;
+    $('#log1').text('You attacked '+enemy.name+' for '+char.attackPower+' damage.');
+    $('#log2').text(enemy.name+' attacked you back for '+enemy.counterAttackPower+' damage.');
+    char.attackPower+=baseIncrease;
+    console.log(char.attackPower)
+    if(enemy.healthPoints<=0){
+        $('#log1').text('You have defeated '+enemy.name+', you can choose to fight another enemy.');
+        $('#log2').text('');
+        $('#defender').find('.character').remove();
+        enemyChosen=false;
+        enemiesDefeated++;
+    }
+    if(char.healthPoints<=0){
+        $('#log1').text('You have been defeated... GAME OVER!!!!');
+        $('#log2').html('<button onclick="myFunc()">Restart</button>');
+        enemyChosen=false;
+    }
+    if(enemiesDefeated==3){
+        $('#log1').text('You won!!!! GAME OVER!!!');
+        $('#log2').html('<button onclick="myFunc()">Restart</button>');
+    }
+    $('#defender').find('.hp').text(enemy.healthPoints)
+    $('#yourCharacter').find('.hp').text(char.healthPoints)
+}
+})
+
+})
+function myFunc(){
+    window.location.reload()
+}
